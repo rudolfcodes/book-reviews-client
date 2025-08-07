e08558471e604157f7f7af6469d727465fadb899
Rudolf Cardol <r_cardol@hotmail.com>
2025-08-07 20:01:20 +0100

    Footer changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,8 @@ const Footer = () => {
   return (
     <div className="h-32 bg-blue-cream flex justify-center items-center text-base text-white">
       <span className="font-light">
-        © 2024 Book <span className="font-semibold">Reviews</span>
+        © {new Date().getFullYear()} Bookclub{" "}
+        <span className="font-semibold">CH</span>
       </span>
     </div>
   );
