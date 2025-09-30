78d017198ea889c5218373d21f353c9119dfb7df
Rudolf Cardol <r_cardol@hotmail.com>
2025-09-30 16:57:21 +0100

    fix css import

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,5 +1,5 @@
 import UserProvider from "@/context/UserProvider";
-import "./globals.css";
+import "../globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
