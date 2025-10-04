e04f2382ba01e446339c4edb942059788c40efba
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-04 17:31:00 +0100

    responsive otp input fields

--- src/components/forms/OtpInput.tsx
+++ src/components/forms/OtpInput.tsx
@@ -21,9 +21,10 @@ const OtpInput = ({ onChange, value, className, onPaste }: OtpInputProps) => {
       value={value}
       onChange={(e) => onChange(e.target.value)}
       onPaste={handlePaste}
-      className={`border-2 border-blue-cream shadow-dark-input w-24 h-auto text-2xl rounded-md min-h-20 pl-10 ${
-        className || ""
-      }`}
+      className={`border-2 border-blue-cream shadow-dark-input rounded-md text-center text-base p-1 w-14 h-14
+  sm:w-16 sm:h-16 sm:text-xl sm:p-3
+  md:w-20 md:h-20 md:text-2xl md:p-4
+  lg:w-24 lg:h-24 lg:text-2xl lg:p-6 ${className || ""}`}
     />
   );
 };
