0e02324714499b48ea574cd0563c1739d64a1e8d
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-06 10:52:44 +0100

    add yup schema

--- src/components/forms/reset-password/ResetPasswordForm.tsx
+++ src/components/forms/reset-password/ResetPasswordForm.tsx
@@ -6,6 +6,23 @@ import { useForm } from "react-hook-form";
 import { toast, ToastContainer } from "react-toastify";
 import FormInput from "../FormInput";
 import BaseButton from "@/components/buttons/BaseButton";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object().shape({
+  oldPassword: yup.string().required("Old password is required"),
+  newPassword: yup
+    .string()
+    .required("New password is required")
+    .min(8, "Password must be at least 8 characters")
+    .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+    .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .matches(/[0-9]/, "Password must contain at least one number")
+    .matches(
+      /[!@#$%^&*]/,
+      "Password must contain at least one special character"
+    ),
+});
 
 interface ResetPasswordInput {
   oldPassword: string;
@@ -25,7 +42,9 @@ const ResetPasswordForm = () => {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<ResetPasswordInput>();
+  } = useForm<ResetPasswordInput>({
+    resolver: yupResolver(schema),
+  });
 
   useEffect(() => {
     const verifyToken = async () => {
