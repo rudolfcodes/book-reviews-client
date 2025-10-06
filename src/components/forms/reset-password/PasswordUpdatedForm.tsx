b63782645fb00f4fc766c1a9067ae4cca35041a8
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-06 12:04:39 +0100

    update button styles

--- src/components/forms/reset-password/PasswordUpdatedForm.tsx
+++ src/components/forms/reset-password/PasswordUpdatedForm.tsx
@@ -3,6 +3,7 @@
 import BaseButton from "@/components/buttons/BaseButton";
 import React from "react";
 import { useRouter } from "next/navigation";
+import FlexContainer from "@/components/FlexContainer";
 
 const PasswordUpdatedForm = () => {
   const router = useRouter();
@@ -15,9 +16,15 @@ const PasswordUpdatedForm = () => {
           Congratulations! Your password has been successfully reset.
         </span>
 
-        <BaseButton type="button" onClick={() => router.push("/auth/login")}>
-          Back to Login
-        </BaseButton>
+        <FlexContainer className="mx-auto xs:w-full md:w-3/4 lg:w-2/3 my-8">
+          <BaseButton
+            className="bg-blue-cream hover:bg-blue-cream hover:scale-105 text-white text-[20px] font-medium w-full min-h-0 mt-6 mb-8 rounded-md h-[60px]"
+            type="button"
+            onClick={() => router.push("/auth/login")}
+          >
+            Back to Login
+          </BaseButton>
+        </FlexContainer>
       </div>
     </div>
   );
