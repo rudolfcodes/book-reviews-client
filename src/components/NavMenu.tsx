918913a1778dbfaf78e3181a5ef9457fc105874e
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-07 22:06:11 +0100

    create navmenu

---
+++ src/components/NavMenu.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface NavmenuProps {
+  items: NavMenuItem[];
+  className?: string;
+}
+
+interface NavMenuItem {
+  label: string;
+  href: string;
+}
+
+const NavMenu = ({ items, className }: NavmenuProps) => {
+  return (
+    <nav className={className}>
+      <ul className="flex space-x-4">
+        {items.map((item, index) => (
+          <li key={index}>
+            <a href={item.href} className="text-gray-700 hover:text-blue-500">
+              {item.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavMenu;
