a2eec500b6ba823c4c759131c370f4d8b08d9c7d
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-13 15:36:54 +0100

    add subtitle to SearchClubs

--- src/components/SearchClubs.tsx
+++ src/components/SearchClubs.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import FlexContainer from "./FlexContainer";
 import TitleContainer from "./TitleContainer";
+import TextContainer from "./TextContainer";
 
-type Props = {};
-
-const SearchClubs = (props: Props) => {
+const SearchClubs = () => {
   return (
-    <FlexContainer className="flex-col">
+    <FlexContainer className="flex-col md:max-w-[455px]">
       <TitleContainer
         title="Find a club near you, or start your own."
-        className="mb-8 openSans-title"
+        className="mb-8 font-openSans"
         coloredText={{ text: "Clubs", color: "hero" }}
+      />
+      <TextContainer
+        className="font-openSans text-base"
+        text="Browse clubs by city, language, and genre. RSVP in two clicks."
       />
     </FlexContainer>
   );
