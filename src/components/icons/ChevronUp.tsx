0f18fa9e7c25ac91005b6f31fdab5db93bc61938
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-16 20:19:01 +0100

    create Icon components

---
+++ src/components/icons/ChevronUp.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+const ChevronUpIcon = (props: any) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={10}
+    height={6}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#1D1B20"
+      d="M5 2.55 1.55 6 .5 4.95 5 .45l4.5 4.5L8.45 6 5 2.55Z"
+    />
+  </svg>
+);
+export default ChevronUpIcon;
