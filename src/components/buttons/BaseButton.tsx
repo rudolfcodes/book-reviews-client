78aaa600a13412fad48cf35e89a60017b0fe805e
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-16 18:58:39 +0100

    Expand BaseButton with a11y
    compatibility - a11y ready for SelectDropdown

--- src/components/buttons/BaseButton.tsx
+++ src/components/buttons/BaseButton.tsx
@@ -3,12 +3,20 @@ import React from "react";
 interface BaseButtonProps {
   type: "button" | "submit" | "reset";
   onClick?: () => void;
+  id?: string;
+  ariaLabel?: string;
+  ariaHasPopup?: boolean;
+  ariaExpanded?: boolean;
   disabled?: boolean;
   className?: string;
   children: React.ReactNode;
 }
 
 const BaseButton = ({
+  id,
+  ariaLabel,
+  ariaHasPopup,
+  ariaExpanded,
   type,
   onClick,
   disabled,
@@ -17,10 +25,14 @@ const BaseButton = ({
 }: BaseButtonProps) => {
   return (
     <button
+      id={id}
       type={type}
       onClick={onClick}
       disabled={disabled}
       className={`btn ${className}`}
+      aria-label={ariaLabel}
+      aria-haspopup={ariaHasPopup}
+      aria-expanded={ariaExpanded}
     >
       {children}
     </button>
