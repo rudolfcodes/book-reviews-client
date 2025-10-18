9436ec2d07d3d1a07de681729ff11acb399aa03e
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-18 15:03:48 +0100

    fix stats data

--- src/data/statsData.ts
+++ src/data/statsData.ts
@@ -1,7 +1,7 @@
 const stats = [
-  { icon: "/icons/clubs.png", value: "137" },
-  { icon: "/icons/members.png", value: "2.1k" },
-  { icon: "/icons/cities.png", value: "18" },
+  { icon: "/icons/books.png", value: "137 clubs" },
+  { icon: "/icons/members.png", value: "2.1k members" },
+  { icon: "/icons/map.png", value: "18 cities" },
 ];
 
 export default stats;
