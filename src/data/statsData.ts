87004e02ab57507eed60442e3b76328ac746551d
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-18 13:56:55 +0100

    create stats data

---
+++ src/data/statsData.ts
@@ -0,0 +1,7 @@
+const stats = [
+  { icon: "/icons/clubs.png", value: "137" },
+  { icon: "/icons/members.png", value: "2.1k" },
+  { icon: "/icons/cities.png", value: "18" },
+];
+
+export default stats;
