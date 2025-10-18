876ff3e7ae53a623f446d700abb745a624877a84
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-18 15:54:29 +0100

    fix Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,8 @@
 import React from "react";
 import Link from "next/link";
 import useUser from "@/hooks/useUser";
+import Navbar from "./Navbar";
+import InnerWrapper from "./InnerWrapper";
 
 const Header = () => {
   // Header should have: logo, current user avatar on the right, and links to sign up and login if no user is logged in
@@ -10,59 +12,56 @@ const Header = () => {
   const { user, logout } = useUser();
 
   return (
-    <header className="flex w-full h-[100px] bg-blue-cream sticky top-0 z-50 justify-between items-center px-[40px]">
-      <div>
-        <Link href="/" className="text-3xl font-bold text-white ml-8">
-          Bookclub CH
-        </Link>
-      </div>
+    <Navbar>
       <div className="h-full max-w-desktop flex justify-between items-center font-raleway">
-        {!user ? (
-          <div className="flex gap-4">
-            <Link
-              className="px-6 py-2 text-sm font-medium border border-gray-300 rounded-lg bg-white text-gray-700 hover:bg-gray-50 hover:scale-105 transition-all duration-200 transform"
-              href="/auth/login"
-            >
-              Log in
-            </Link>
-            <Link
-              className="px-6 py-2 text-sm font-medium rounded-lg bg-gray-900 text-white hover:bg-gray-700 hover:scale-105 transition-all duration-200 transform"
-              href="/auth/register"
-            >
-              Join community
-            </Link>
-          </div>
-        ) : (
-          <div className="flex text-xl text-white gap-9">
-            <div className="dropdown dropdown-end">
-              <div tabIndex={0} role="button" className="avatar placeholder">
-                <div className="bg-modern-accent w-11 rounded-full">
-                  <span>{user.username.charAt(0).toUpperCase()}</span>
-                </div>
-              </div>
-              <ul className="dropdown-content menu min-w-[180px] bg-white font-semibold text-base text-blue-cream rounded-box rounded-bl-none rounded-br-none w-fit border-2 p-0 mt-1 border-blue-cream">
-                <li className="text-blue-cream hover:text-white rounded-none">
-                  <a
-                    className="dropdown-item pl-4 hover:bg-blue-cream rounded-bl-none rounded-br-none focus:bg-transparent active:bg-transparent"
-                    href="/profile"
-                  >
-                    Profile settings
-                  </a>
-                </li>
-                <li className="text-blue-cream hover:text-white rounded-none">
-                  <button
-                    className="dropdown-item pl-4 hover:bg-blue-cream rounded-none focus:bg-transparent active:bg-transparent"
-                    onClick={logout}
-                  >
-                    Log out
-                  </button>
-                </li>
-              </ul>
+        <InnerWrapper>
+          {!user ? (
+            <div className="flex gap-4">
+              <Link
+                className="px-6 py-2 text-sm font-medium border border-gray-300 rounded-lg bg-white text-gray-700 hover:bg-gray-50 hover:scale-105 transition-all duration-200 transform"
+                href="/auth/login"
+              >
+                Log in
+              </Link>
+              <Link
+                className="px-6 py-2 text-sm font-medium rounded-lg bg-gray-900 text-white hover:bg-gray-700 hover:scale-105 transition-all duration-200 transform"
+                href="/auth/register"
+              >
+                Join community
+              </Link>
             </div>
-          </div>
-        )}
+          ) : (
+            <div className="flex text-xl text-white gap-9">
+              <div className="dropdown dropdown-end">
+                <div tabIndex={0} role="button" className="avatar placeholder">
+                  <div className="bg-modern-accent w-11 rounded-full">
+                    <span>{user.username.charAt(0).toUpperCase()}</span>
+                  </div>
+                </div>
+                <ul className="dropdown-content menu min-w-[180px] bg-white font-semibold text-base text-blue-cream rounded-box rounded-bl-none rounded-br-none w-fit border-2 p-0 mt-1 border-blue-cream">
+                  <li className="text-blue-cream hover:text-white rounded-none">
+                    <a
+                      className="dropdown-item pl-4 hover:bg-blue-cream rounded-bl-none rounded-br-none focus:bg-transparent active:bg-transparent"
+                      href="/profile"
+                    >
+                      Profile settings
+                    </a>
+                  </li>
+                  <li className="text-blue-cream hover:text-white rounded-none">
+                    <button
+                      className="dropdown-item pl-4 hover:bg-blue-cream rounded-none focus:bg-transparent active:bg-transparent"
+                      onClick={logout}
+                    >
+                      Log out
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          )}
+        </InnerWrapper>
       </div>
-    </header>
+    </Navbar>
   );
 };
 
