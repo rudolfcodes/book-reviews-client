b8baee0a480fa4c3a1f4e63a30065882373e6879
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-21 13:20:44 +0100

    use hook

--- src/components/ListPopularClubs.tsx
+++ src/components/ListPopularClubs.tsx
@@ -1,17 +1,31 @@
+"use client";
+
 import { ClubEntity } from "@/types";
 import React from "react";
 import BaseCard from "./cards/BaseCard";
+import { useFetchPopularClubs } from "@/hooks/useFetchPopularClubs";
 
-type PopularClub = Pick<
-  ClubEntity,
-  "_id" | "name" | "memberCount" | "location" | "clubImage"
->;
+const ListPopularClubs = () => {
+  const { popularClubs, isLoading, isError, refetch } = useFetchPopularClubs();
+  console.log("Popular Clubs:", popularClubs);
 
-const ListPopularClubs = ({
-  popularClubs,
-}: {
-  popularClubs: PopularClub[];
-}) => {
+  if (isLoading) {
+    return <div>Loading popular clubs...</div>;
+  }
+
+  if (isError) {
+    return (
+      <div>
+        Error loading popular clubs.{" "}
+        <button onClick={() => refetch()}>Retry</button>
+      </div>
+    );
+  }
+
+  if (!popularClubs || popularClubs.length === 0) {
+    return <div>No popular clubs found.</div>;
+  }
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       {popularClubs.map((club) => (
@@ -19,7 +33,7 @@ const ListPopularClubs = ({
           <img
             src={club.clubImage}
             alt={club.name}
-            className="w-full h-32 object-cover rounded"
+            className="w-full h-32 object-cover rounded-tl-xl rounded-tr-xl mb-4"
           />
           <h3 className="text-lg font-semibold">{club.name}</h3>
 
