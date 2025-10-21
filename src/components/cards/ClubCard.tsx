513d44209241d6fd313d65e5bfb8d5e8826478a5
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-21 19:55:43 +0100

    created ClubCard

---
+++ src/components/cards/ClubCard.tsx
@@ -0,0 +1,68 @@
+import { ClubSimplified } from "@/types";
+import React from "react";
+import BaseCard from "./BaseCard";
+import FlexContainer from "../FlexContainer";
+import BaseButton from "../buttons/BaseButton";
+import Link from "next/link";
+import Badge from "../Badge";
+
+const ClubCard = ({
+  _id,
+  name,
+  location,
+  imageUrl,
+  description,
+  language,
+  members,
+  meetingFrequency,
+  meetingTime,
+}: ClubSimplified) => {
+  const badges = [
+    meetingTime,
+    location.city,
+    `${members.length} members`,
+    language,
+    meetingFrequency,
+  ];
+
+  return (
+    <div>
+      <BaseCard key={_id}>
+        <img
+          src={imageUrl || "/images/default-club.jpg"}
+          alt={name}
+          className="w-full h-32 object-cover rounded-tl-xl rounded-tr-xl lg:h-40"
+        />
+
+        <FlexContainer className="px-7 py-8 flex-col">
+          <h3 className="text-card-title font-medium mb-2">{name}</h3>
+
+          {/* Badges */}
+          <FlexContainer className="text-gray-600 text-xs gap-4 flex-wrap">
+            {badges.map((badge, index) => (
+              <Badge
+                key={`${_id}`}
+                variant={index === 0 ? "success" : "default"}
+              >
+                <span>{badge}</span>
+              </Badge>
+            ))}
+          </FlexContainer>
+          {/* Description */}
+          <p className="text-gray-500 text-sm mt-4 line-clamp-3">
+            {description}
+          </p>
+          <BaseButton
+            type="button"
+            className="mt-6 w-full bg-error text-white h-10 rounded-none font-medium hover:scale-105 hover:bg-error transition-colors duration-200 border-none"
+          >
+            Join
+          </BaseButton>
+          <Link href={`/clubs/${_id}`}>Details</Link>
+        </FlexContainer>
+      </BaseCard>
+    </div>
+  );
+};
+
+export default ClubCard;
