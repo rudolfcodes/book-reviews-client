8a7bb78862933097d80c8d9ebb9f9bfb122495a0
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-21 21:50:21 +0100

    align cards

--- src/components/cards/ClubCard.tsx
+++ src/components/cards/ClubCard.tsx
@@ -26,45 +26,46 @@ const ClubCard = ({
   ];
 
   return (
-    <div>
-      <BaseCard key={_id}>
-        <img
-          src={imageUrl || "/images/default-club.jpg"}
-          alt={name}
-          className="w-full h-32 object-cover rounded-tl-xl rounded-tr-xl lg:h-40"
-        />
+    <BaseCard key={_id} className="min-h-[563px] justify-between">
+      <img
+        src={imageUrl || "/images/default-club.jpg"}
+        alt={name}
+        className="w-full h-32 object-cover rounded-tl-xl rounded-tr-xl lg:h-40"
+      />
 
-        <FlexContainer className="flex-col gap-2 px-7 py-8 ">
-          <h3 className="text-card-title font-medium mb-2 text-ellipsis overflow-hidden whitespace-nowrap">
-            {name}
-          </h3>
+      <FlexContainer className="flex-col gap-2 px-7 py-8 flex-1">
+        <h3 className="text-card-title font-medium mb-2 text-ellipsis overflow-hidden whitespace-nowrap">
+          {name}
+        </h3>
 
-          <FlexContainer className="text-gray-600 text-xs gap-4 flex-wrap">
-            {badges.map((badge, index) => (
-              <Badge
-                key={`${_id}`}
-                variant={index === 0 ? "success" : "default"}
-              >
-                <span>{badge}</span>
-              </Badge>
-            ))}
-          </FlexContainer>
+        <FlexContainer className="text-gray-600 text-xs gap-4 flex-wrap">
+          {badges.map((badge, index) => (
+            <Badge key={`${_id}`} variant={index === 0 ? "success" : "default"}>
+              <span>{badge}</span>
+            </Badge>
+          ))}
+        </FlexContainer>
 
-          <p className="text-gray-500 text-sm mt-4 line-clamp-3">
-            {description}
-          </p>
+        <p className="text-gray-500 text-sm mt-4 line-clamp-5 min-h-[100px] flex-grow">
+          {description}
+        </p>
+
+        <div className="mt-auto pt-6">
           <BaseButton
             type="button"
-            className="mt-6 w-full bg-error text-white h-10 rounded-xl font-medium hover:scale-105 hover:bg-error transition-all duration-200 border-none"
+            className="w-full bg-error text-white h-10 rounded-xl font-medium hover:scale-105 hover:bg-error transition-all duration-200 border-none mb-4"
           >
             Join
           </BaseButton>
-          <Link className="flex justify-center" href={`/clubs/${_id}`}>
+          <Link
+            className="flex justify-center font-openSans text-primary-grey text-xs underline"
+            href={`/clubs/${_id}`}
+          >
             Details
           </Link>
-        </FlexContainer>
-      </BaseCard>
-    </div>
+        </div>
+      </FlexContainer>
+    </BaseCard>
   );
 };
 
