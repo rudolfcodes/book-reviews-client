96b6c526c80e355712711c00512c5323cd7b511a
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-22 21:53:02 +0100

    fix queryClient

--- src/hooks/useJoinClub.ts
+++ src/hooks/useJoinClub.ts
@@ -1,15 +1,18 @@
 import { joinClub } from "@/services/clubService";
 import { useMutation } from "@tanstack/react-query";
-import { QueryClient } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
+import { useQueryClient } from "@tanstack/react-query";
 
 const useJoinClub = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (clubId: string) => joinClub(clubId),
     onSuccess: () => {
       // Invalidate and refetch
+      console.log("Invalidating queries...");
       queryClient.invalidateQueries({ queryKey: ["clubs"] });
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+      console.log("Queries invalidated");
     },
   });
 };
