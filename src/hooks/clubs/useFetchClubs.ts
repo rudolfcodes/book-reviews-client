a5f2213d0c76b1b0e8a6371a5918091339ed315c
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-22 15:55:42 +0100

    add ClubsAPI response

--- src/hooks/clubs/useFetchClubs.ts
+++ src/hooks/clubs/useFetchClubs.ts
@@ -2,16 +2,28 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchClubs } from "@/services/clubService";
 import { ClubSimplified } from "@/types";
 
+type ClubsApiResponse = {
+  data: {
+    docs: ClubSimplified[];
+    totalDocs?: number;
+    limit?: number;
+    page?: number;
+    hasNextPage?: boolean;
+  };
+};
+
 const useFetchPopularClubs = () => {
-  const clubsQuery = useQuery<{ data: ClubSimplified[] }>({
+  const clubsQuery = useQuery<ClubsApiResponse>({
     queryKey: ["clubs", { sortBy: "popular", limit: 3 }],
     queryFn: () => fetchClubs({ sortBy: "popular", limit: 3 }),
     staleTime: 10 * 60 * 1000,
     refetchOnWindowFocus: false,
   });
 
+  console.log({ clubsQuery });
+
   return {
-    popularClubs: clubsQuery.data?.data || [],
+    popularClubs: clubsQuery.data?.data?.docs || [],
     isLoading: clubsQuery.isLoading,
     isError: clubsQuery.isError,
     refetch: clubsQuery.refetch,
@@ -19,13 +31,13 @@ const useFetchPopularClubs = () => {
 };
 
 const useFetchNewestClubs = () => {
-  const clubsQuery = useQuery<{ data: ClubSimplified[] }>({
+  const clubsQuery = useQuery<ClubsApiResponse>({
     queryKey: ["clubs", { sortBy: "newest", limit: 10 }],
     queryFn: () => fetchClubs({ sortBy: "newest", limit: 10 }),
   });
 
   return {
-    newestClubs: clubsQuery.data?.data || [],
+    newestClubs: clubsQuery.data?.data?.docs || [],
     isLoading: clubsQuery.isLoading,
     isError: clubsQuery.isError,
     refetch: clubsQuery.refetch,
