557b4a4c6eabbb3882f97f9aa7ce2be3d70569ce
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-24 20:39:16 +0100

    align everything properly

--- src/components/ListEvents.tsx
+++ src/components/ListEvents.tsx
@@ -20,31 +20,33 @@ const ListEvents = () => {
   }
 
   return (
-    <InnerWrapper className="bg-hero bg-opacity-15 flex-col my-32 gap-20">
-      <h1 className="font-openSans text-section-title font-semiBold mb-4">
-        Happening this week near you
-      </h1>
+    <section className="bg-hero flex py-section-lg gap-20 bg-opacity-15">
+      <InnerWrapper className="flex flex-col">
+        <h1 className="font-openSans text-section-title font-semiBold mb-12">
+          Happening this week near you
+        </h1>
 
-      {/* Event filter badges: Today, This week, Online, In-person */}
-      <FlexContainer className="justify-between items-center">
-        <FlexContainer className="gap-4">
-          <Badge variant="default">Today</Badge>
-          <Badge variant="default">This week</Badge>
-          <Badge variant="default">Online</Badge>
-          <Badge variant="default">In-person</Badge>
+        {/* Event filter badges: Today, This week, Online, In-person */}
+        <FlexContainer className="justify-between items-center mb-24 lg:w-[45%]">
+          <FlexContainer className="gap-4">
+            <Badge variant="default">Today</Badge>
+            <Badge variant="default">This week</Badge>
+            <Badge variant="default">Online</Badge>
+            <Badge variant="default">In-person</Badge>
+          </FlexContainer>
+
+          <Link href="/events" className="text-sm font-openSans text-black">
+            View all events &rarr;
+          </Link>
         </FlexContainer>
 
-        <Link href="/events" className="text-sm font-openSans text-black">
-          View all events &rarr;
-        </Link>
-      </FlexContainer>
-
-      <div className="grid grid-cols-3 tablet:grid-cols-2 mobile:grid-cols-1">
-        {events.map((event) => (
-          <EventCard key={event._id} {...event} />
-        ))}
-      </div>
-    </InnerWrapper>
+        <div className="grid grid-cols-3 tablet:grid-cols-2 mobile:grid-cols-1 gap-5">
+          {events.map((event) => (
+            <EventCard key={event._id} {...event} />
+          ))}
+        </div>
+      </InnerWrapper>
+    </section>
   );
 };
 
