aa704409fb06a2596d5ddec404ea81e09dd54eea
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-29 16:52:06 +0000

    fix button styles

--- src/components/CTABanner.tsx
+++ src/components/CTABanner.tsx
@@ -34,11 +34,15 @@ const CTABanner = ({
         className || ""
       } gap-4 text-center flex-col py-12 bg-[#F9FAFB]`}
     >
-      <InnerWrapper className="flex flex-col gap-5">
+      <InnerWrapper className="flex flex-col gap-5 !md:max-w-sm lg:max-w-[450px]">
         {icon && <div>{icon}</div>}
-        <h2 className="text-label">{title}</h2>
+        <h2 className="text-label text-banner-title">{title}</h2>
         {subtitle && <h3>{subtitle}</h3>}
-        <BaseButton type="button" onClick={() => router.push(cta.href)}>
+        <BaseButton
+          className="bg-error hover:bg-error/80 hover:scale-110 transition-all duration-200 text-white font-semiBold rounded-xl px-14 py-2 hover:border-none"
+          type="button"
+          onClick={() => router.push(cta.href)}
+        >
           {cta.buttonText}
         </BaseButton>
       </InnerWrapper>
