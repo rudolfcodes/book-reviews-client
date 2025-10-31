23920a6e83b6b95950c35ee9076c754307e3b37d
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-31 22:01:20 +0000

    add responsive styles

--- src/components/ChatTeaser.tsx
+++ src/components/ChatTeaser.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import InnerWrapper from "./InnerWrapper";
-import TitleContainer from "./TitleContainer";
 import TextContainer from "./TextContainer";
 import BaseButton from "./buttons/BaseButton";
 import LinkIcon from "./icons/LinkIcon";
@@ -14,13 +13,12 @@ const ChatTeaser = () => {
   const router = useRouter();
 
   return (
-    <InnerWrapper>
-      <FlexContainer className="py-section-lg">
-        <FlexContainer className="flex-col justify-center gap-6">
-          <TitleContainer
-            title="Plan together, without messy group texts"
-            className="banner-title"
-          />
+    <InnerWrapper className="px-10 lg:!px-10 xl:px-4">
+      <FlexContainer className="py-10 lg:py-section-lg relative w-full h-[400px] lg:h-[600px] xl:h-[790px] lg:flex-row flex-col items-center">
+        <FlexContainer className="w-full justify-center items-center text-center relative md:text-left md:absolute flex-col gap-6 md:inset-y-0 md:left-0 md:right-1/4 lg:items-start">
+          <h2 className="text-banner-title lg:text-big-title xl:text-section-title-xl w-3/4 xl:w-[720px]">
+            Plan together, without messy group texts
+          </h2>
           <TextContainer
             text="Message hosts for logistics, coordinate with attendees, and get reminders only when plans change."
             className="subtitle lg:max-w-[720px]"
@@ -38,7 +36,7 @@ const ChatTeaser = () => {
         <NextImage
           src="/images/chat-teaser.png"
           alt="Chat Preview"
-          className="chat-preview -mb-32"
+          className="hidden lg:block absolute lg:w-1/2 xl:w-fit right-0 bottom-0 items-center z-10 chat-preview"
           width={720}
           height={400}
         />
