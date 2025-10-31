0a7ed741b8dd38c319c5c9de72c28a467e34097f
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-31 14:18:37 +0000

    add width & height to Image

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -4,11 +4,13 @@ import Image from "next/image";
 interface ImageProps {
   src: string;
   alt: string;
+  width: number;
+  height: number;
   className?: string;
 }
 
-const NextImage = ({ src, alt, className }: ImageProps) => {
-  return <Image src={src} alt={alt} className={className} />;
+const NextImage = ({ src, alt, width, height, className }: ImageProps) => {
+  return <Image src={src} alt={alt} width={width} height={height} className={className} />;
 };
 
 export default NextImage;
