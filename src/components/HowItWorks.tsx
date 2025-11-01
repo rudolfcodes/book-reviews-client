46392b24a0dd39903c71ba1468e756fa1a786896
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-01 11:51:58 +0000

    How it works section

---
+++ src/components/HowItWorks.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import InnerWrapper from "./InnerWrapper";
+import FlexContainer from "./FlexContainer";
+import ListStepCards from "./cards/ListStepCards";
+import { steps } from "@/data/steps";
+
+const HowItWorks = () => {
+  return (
+    <FlexContainer className="bg-off-white pt-16 pb-20">
+      <InnerWrapper>
+        <h2 className="text-banner-title">
+          Get started with{" "}
+          <span className="text-error text-banner-title">SWISS Bookclub</span>
+        </h2>
+
+        <ListStepCards steps={steps} />
+      </InnerWrapper>
+    </FlexContainer>
+  );
+};
+
+export default HowItWorks;
