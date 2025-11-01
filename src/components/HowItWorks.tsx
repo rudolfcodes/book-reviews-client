1099d67bc65d94582d9c0447c0ee8f7c53a3e2c2
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-01 12:12:03 +0000

    center title

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -8,7 +8,7 @@ const HowItWorks = () => {
   return (
     <FlexContainer className="bg-off-white pt-16 pb-20">
       <InnerWrapper className="flex-col gap-[72px]">
-        <h2 className="text-banner-title">
+        <h2 className="text-banner-title text-center">
           Get started with{" "}
           <span className="text-error text-banner-title">SWISS Bookclub</span>
         </h2>
