9baa6d070fe146d1d84b3d2841514bd35994760e
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-01 13:58:22 +0000

    add social icons

---
+++ src/components/icons/FacebookIcon.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+const FacebookIcon = (props: any) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={20}
+    height={20}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#fff"
+      d="M19.375 9.688c0 4.843-3.555 8.867-8.203 9.57V12.5h2.265l.43-2.813h-2.695V7.891c0-.782.39-1.524 1.601-1.524h1.211V3.984s-1.093-.195-2.187-.195c-2.188 0-3.633 1.367-3.633 3.79v2.109h-2.46V12.5h2.46v6.758C3.516 18.555 0 14.53 0 9.688A9.686 9.686 0 0 1 9.688 0a9.686 9.686 0 0 1 9.687 9.688Z"
+    />
+  </svg>
+);
+export default FacebookIcon;
