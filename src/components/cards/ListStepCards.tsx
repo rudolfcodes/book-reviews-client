e188d311005de39f63d5a34b711937aee456a7d0
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-01 11:51:50 +0000

    component for listing steps

---
+++ src/components/cards/ListStepCards.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ListStepCardsProps } from "@/types/step/step.types";
+import StepCard from "./StepCard";
+
+const ListStepCards = ({ steps, className }: ListStepCardsProps) => {
+  return (
+    <div className={className}>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7">
+        {steps.map((step) => (
+          <StepCard key={step.stepNumber} {...step} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ListStepCards;
