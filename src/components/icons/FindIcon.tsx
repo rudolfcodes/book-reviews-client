747da7d87644342470ff5092e171cee891327047
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-01 10:56:48 +0000

    create icons for process step cards

---
+++ src/components/icons/FindIcon.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+const FindIcon = (props: any) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={25}
+    height={25}
+    fill="none"
+    {...props}
+  >
+    <path
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="m24 24-5.545-5.545M11.222 21.444c5.646 0 10.222-4.576 10.222-10.222C21.444 5.577 16.868 1 11.222 1 5.577 1 1 5.577 1 11.222c0 5.646 4.577 10.222 10.222 10.222Z"
+    />
+  </svg>
+);
+export default FindIcon;
---
+++ src/components/icons/JoinIcon.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+const JoinIcon = (props: any) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={22}
+    height={21}
+    fill="none"
+    {...props}
+  >
+    <path
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M1 19a8 8 0 0 1 13.292-6"
+    />
+    <path
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M9 11A5 5 0 1 0 9 1a5 5 0 0 0 0 10ZM18 14v6m3-3h-6"
+    />
+  </svg>
+);
+export default JoinIcon;
