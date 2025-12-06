806e88c306e80a17908cbc41fe86e8f7f3a0c3cc
Rudolf Cardol <r_cardol@hotmail.com>
2025-12-06 22:08:38 +0000

    add ClubOptionalInfoForm

---
+++ src/components/forms/clubs/create/multistep/ClubOptionalInfoForm.tsx
@@ -0,0 +1,103 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import FlexContainer from "@/components/FlexContainer";
+import TitleContainer from "@/components/TitleContainer";
+import TextContainer from "@/components/TextContainer";
+import FormInput from "@/components/forms/FormInput";
+import ImageUploadIcon from "@/components/icons/ImageUpload";
+import DropdownMenu from "@/components/DropdownMenu";
+import { languages } from "@/data/languages";
+import { useState } from "react";
+
+type ClubOptionalInfoFormProps = {
+  onSubmit: (data: any) => void;
+};
+
+type ClubOptionalInfoInputFormProps = {
+  genre?: string;
+  bookTitle?: string;
+  clubimageUrl?: File;
+  language?: "en" | "de" | "fr" | "it";
+  location?: "online" | "in-person" | "hybrid";
+};
+
+const schema: yup.ObjectSchema<ClubOptionalInfoInputFormProps> = yup.object({
+  genre: yup.string().max(50, "Genre must be at most 50 characters"),
+  bookTitle: yup
+    .string()
+    .min(2)
+    .max(100, "Book title must be at most 100 characters"),
+  clubimageUrl: yup
+    .mixed<File>()
+    .test("fileType", "File size is too large", (value) => {
+      if (!value) return true;
+      const maxSize = 5 * 1024 * 1024;
+
+      if (value.size > maxSize) return false;
+      return true;
+    }),
+  language: yup
+    .string<"en" | "de" | "fr" | "it">()
+    .oneOf(["en", "de", "fr", "it"], "Invalid language selection"),
+  location: yup
+    .string<"online" | "in-person" | "hybrid">()
+    .oneOf(["online", "in-person", "hybrid"], "Invalid location selection"),
+});
+
+const ClubOptionalInfoForm = ({ onSubmit }: ClubOptionalInfoFormProps) => {
+  const [selectedLanguage, setSelectedLanguage] = useState<string>("English");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClubOptionalInfoInputFormProps>({
+    resolver: yupResolver(schema),
+  });
+
+  return (
+    <FlexContainer className="w-full flex flex-col gap-6 mt-6">
+      <TitleContainer
+        className="text-black mb-4"
+        title="Complete your club information"
+      />
+      <TextContainer text="Add a nice image, genre and explain where the club takes place" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormInput
+          label="Genre:"
+          type="text"
+          register={register("genre")}
+          error={errors.genre?.message}
+          placeholder="What’s the genre of the discussed books?"
+        />
+        <FormInput
+          label="Book title:"
+          type="text"
+          register={register("bookTitle")}
+          error={errors.bookTitle?.message}
+          placeholder="What book will you be discussing?"
+        />
+        <FlexContainer className="gap-4">
+          <FormInput
+            label="Club image:"
+            type="file"
+            register={register("clubimageUrl")}
+            error={errors.clubimageUrl?.message}
+            placeholder="Upload image"
+            icon={<ImageUploadIcon />}
+          />
+          <DropdownMenu
+            data={languages}
+            ariaLabel="language select dropdown"
+            handleChange={(language) => setSelectedLanguage(language.name)}
+            selectedId={selectedLanguage}
+            id="club-language"
+            hasImage
+          />
+        </FlexContainer>
+      </form>
+    </FlexContainer>
+  );
+};
+
+export default ClubOptionalInfoForm;
