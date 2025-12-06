fa863ed1996de7e83c89d5e2ce8f9e83facf9307
Rudolf Cardol <r_cardol@hotmail.com>
2025-12-06 20:20:48 +0000

    add optional icon to form input

--- src/components/forms/FormInput.tsx
+++ src/components/forms/FormInput.tsx
@@ -9,6 +9,7 @@ interface FormInputProps {
   required?: boolean;
   className?: string;
   placeholder?: string;
+  icon?: React.ReactNode;
 }
 
 const FormInput = ({
@@ -19,18 +20,24 @@ const FormInput = ({
   required,
   className,
   placeholder,
+  icon,
 }: FormInputProps) => {
   return (
     <div className={className}>
       <label className="block text-auth-label">
         {label} {required && <span className="text-red-500">*</span>}
         <input
-          className="input input-bordered w-full h-[60px] rounded-md border-2 border-blue-cream mt-3 shadow-soft-card"
+          className="relative input input-bordered w-full h-[60px] rounded-md border-2 border-blue-cream mt-3 shadow-soft-card"
           type={type}
           {...register}
           required={required}
           placeholder={placeholder}
         />
+        {icon && (
+          <div className="absolute left-4 top-1/2 transform -translate-y-1/2">
+            {icon}
+          </div>
+        )}
       </label>
       {error && <p className="text-error text-sm">{error}</p>}
     </div>
