56dcc4d477cc76f440f30022d958e81817de1435
Rudolf Cardol <r_cardol@hotmail.com>
2025-12-09 14:22:56 +0000

    added test for onDataChange call on input change

--- src/components/forms/clubs/create/multistep/ClubNameForm.test.tsx
+++ src/components/forms/clubs/create/multistep/ClubNameForm.test.tsx
@@ -1,10 +1,7 @@
-// ClubNameForm tests
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import { expect } from "@jest/globals";
 import ClubNameForm from "./ClubNameForm";
-
-// Extend Jest matchers
 import "@testing-library/jest-dom";
 
 describe("ClubNameForm", () => {
@@ -33,4 +30,25 @@ describe("ClubNameForm", () => {
       await screen.findByText(/club name is required/i)
     ).toBeInTheDocument();
   });
+
+  it("calls onDataChange when inputs change", () => {
+    renderComponent();
+    const nameInput = screen.getByLabelText(/club name/i);
+    const descriptionInput = screen.getByLabelText(/description/i);
+    fireEvent.change(nameInput, { target: { value: "Book Lovers" } });
+    fireEvent.change(descriptionInput, {
+      target: { value: "A club for book enthusiasts" },
+    });
+    expect(mockOnDataChange).toHaveBeenCalledWith({
+      name: "Book Lovers",
+      description: "A club for book enthusiasts",
+    });
+  });
+
+  // TODO: Call onValidationChange on validation status change
+
+  // TODO: Save typed values in the form inputs when switching steps
+
+  // TODO: Check that all the data is passed/collected in a formData object in the parent
+  // TODO: That will onSubmit submit the full formData object to the backend in a POST request
 });
