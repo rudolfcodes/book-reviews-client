1f16a6d03529343d2c7f1b712aa7f3f3a2b4a9f0
Rudolf Cardol <r_cardol@hotmail.com>
2025-12-13 22:36:29 +0000

    add reusable getServerAuthToken

---
+++ src/utils/auth.ts
@@ -0,0 +1,35 @@
+import { DecodedToken } from "@/types/token";
+import { jwtDecode } from "jwt-decode";
+import { cookies } from "next/headers";
+
+export async function getServerAuthToken() {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token")?.value;
+
+  if (!token) {
+    return {
+      token: null,
+      isAuthenticated: false,
+      decodedToken: null,
+      userId: null,
+    };
+  }
+
+  try {
+    const decodedToken = jwtDecode<DecodedToken>(token);
+    return {
+      token,
+      decodedToken,
+      isAuthenticated: true,
+      userId: decodedToken.userId,
+    };
+  } catch (error) {
+    console.log("Error verifying token:", error);
+    return {
+      token: null,
+      isAuthenticated: false,
+      decodedToken: null,
+      userId: null,
+    };
+  }
+}
