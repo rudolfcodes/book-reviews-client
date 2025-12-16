b528cb747271fe59b013f9bb8f3fe9342e61cd9b
Rudolf Cardol <r_cardol@hotmail.com>
2025-12-16 11:22:12 +0000

    add membercount, CTA and badges

--- src/components/cards/ClubOverviewCard.tsx
+++ src/components/cards/ClubOverviewCard.tsx
@@ -1,6 +1,10 @@
 import BaseCard from "./BaseCard";
 import Avatar from "../Avatar";
 import { ClubOverviewCardProps } from "@/types/club/club.types";
+import FlexContainer from "../FlexContainer";
+import MembersIcon from "../icons/MembersIcon";
+import BaseButton from "../buttons/BaseButton";
+import Badge from "../Badge";
 
 const ClubOverviewCard = ({
   membersCount,
@@ -11,8 +15,26 @@ const ClubOverviewCard = ({
   avatarUrl,
 }: ClubOverviewCardProps) => {
   return (
-    <BaseCard>
+    <BaseCard className="gap-6">
       <Avatar imageUrl={avatarUrl} size="md" altText="Club Avatar" />
+      <FlexContainer className="gap-2">
+        <MembersIcon />
+        <span>{membersCount !== undefined ? membersCount : "N/A"} members</span>
+      </FlexContainer>
+
+      <BaseButton
+        type="button"
+        className="bg-error text-white px-8 py-3.5 rounded-md lg:w-[212px] h-full"
+        onClick={onJoinClick}
+      >
+        Join Club
+      </BaseButton>
+
+      <FlexContainer className="gap-2 flex-wrap">
+        {genre && <Badge>{genre}</Badge>}
+        {venueType && <Badge>{venueType}</Badge>}
+        {language && <Badge>{language}</Badge>}
+      </FlexContainer>
     </BaseCard>
   );
 };
