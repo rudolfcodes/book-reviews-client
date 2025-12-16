2f4833fbfb829834f22ba89d4b72d14daeca6ca2
Rudolf Cardol <r_cardol@hotmail.com>
2025-12-16 20:49:32 +0000

    fix overview card styles

--- src/components/cards/ClubOverviewCard.tsx
+++ src/components/cards/ClubOverviewCard.tsx
@@ -18,7 +18,7 @@ const ClubOverviewCard = ({
   const { user } = useUser();
 
   return (
-    <BaseCard className="w-full lg:!max-w-[450px] gap-6 flex-shrink-0">
+    <BaseCard className="w-full lg:!max-w-[450px] gap-6 flex-shrink-0 py-10 justify-center items-center border-none shadow-input-shadow">
       <Avatar
         imageUrl={avatarUrl}
         size="md"
@@ -27,12 +27,12 @@ const ClubOverviewCard = ({
       />
       <FlexContainer className="gap-2">
         <MembersIcon />
-        <span>{membersCount !== undefined ? membersCount : "N/A"} members</span>
+        <span>{membersCount !== undefined ? membersCount : "0"} members</span>
       </FlexContainer>
 
       <BaseButton
         type="button"
-        className="bg-error text-white px-8 py-3.5 rounded-md lg:w-[212px] h-full"
+        className="bg-error text-white px-8 py-3.5 rounded-md lg:w-[212px]"
         onClick={onJoinClick}
       >
         Join Club
