06e7cf9bd6c8485da31016248f7396370e71acf0
Rudolf Cardol <r_cardol@hotmail.com>
2025-12-18 21:37:14 +0000

    add fetchUserByClubId to service

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,8 +1,24 @@
 import axiosInstance from "@/utils/axios";
+import { AxiosError } from "axios";
 
 const fetchCurrentUser = async () => {
   const response = await axiosInstance.get("/api/users/me");
   return response.data;
 };
 
-export { fetchCurrentUser };
+const fetchUserByClubId = async (clubId: string) => {
+  try {
+    const response = await axiosInstance.get(`/api/users/${clubId}`);
+    return response.data;
+  } catch (error: unknown) {
+    if (error instanceof AxiosError && error.response) {
+      console.error(
+        "Something went wrong fetching the user: ",
+        error.response?.data?.message || error.message
+      );
+    }
+    console.error("Something went wrong fetching the user: ", error);
+  }
+};
+
+export { fetchCurrentUser, fetchUserByClubId };
