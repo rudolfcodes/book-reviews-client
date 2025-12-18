f68b57c04bd59e031046719b54e5faf86e41cbab
Rudolf Cardol <r_cardol@hotmail.com>
2025-12-18 14:44:11 +0000

    add OrganisedBy

---
+++ src/components/OrganisedBy.tsx
@@ -0,0 +1,41 @@
+import Avatar from "./Avatar";
+import BaseButton from "./buttons/BaseButton";
+import BaseCard from "./cards/BaseCard";
+import FlexContainer from "./FlexContainer";
+import TextContainer from "./TextContainer";
+import TitleContainer from "./TitleContainer";
+
+interface OrganisedByProps {
+  hostName: string;
+  messageOnClick?: string;
+  hostAvatarUrl?: string;
+}
+
+const OrganisedBy = ({
+  hostName,
+  messageOnClick,
+  hostAvatarUrl,
+}: OrganisedByProps) => {
+  return (
+    <BaseCard className="flex-row w-full lg:!max-w-[450px] gap-6 flex-shrink-0 py-10 justify-between items-center border-none shadow-input-shadow">
+      <FlexContainer className="flex-col gap-4 items-center">
+        <TitleContainer title="Organised by:" />
+        <TextContainer text={hostName} className="text-lg font-semibold" />
+
+        <BaseButton
+          type="button"
+          className="bg-modern-tertiary text-white"
+          onClick={() => messageOnClick}
+        >
+          Message Host
+        </BaseButton>
+      </FlexContainer>
+
+      {hostAvatarUrl && (
+        <Avatar imageUrl={hostAvatarUrl} altText={`${hostName}'s avatar`} />
+      )}
+    </BaseCard>
+  );
+};
+
+export default OrganisedBy;
