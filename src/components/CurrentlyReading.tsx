04e92fcff30e7b0f754d34a17887c295c4835658
Rudolf Cardol <r_cardol@hotmail.com>
2025-12-20 11:32:35 +0000

    use BaseCard & NextImage in CurrentlyReading

--- src/components/CurrentlyReading.tsx
+++ src/components/CurrentlyReading.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
 import FlexContainer from "./FlexContainer";
 import TitleContainer from "./TitleContainer";
 import { Book } from "@/types/book/book.types";
+import BaseCard from "./cards/BaseCard";
+import NextImage from "./Image";
 
 interface CurrentlyReadingProps {
   book: Book;
@@ -15,16 +16,21 @@ const CurrentlyReading = ({ book }: CurrentlyReadingProps) => {
         className="text-font24 font-bold"
       />
 
-      <div className="relative border-10 border-white rounded-xl shadow-input-shadow">
+      <BaseCard className="relative border-10 border-white rounded-xl shadow-input-shadow">
         <div className="absolute top-right bg-black text-white px-2 py-1 border-t">
           NOW
         </div>
-        <Image src={book.src} alt={book.title} width={120} height={180} />
+        <NextImage
+          src={book.coverUrl}
+          alt={book.title}
+          width={120}
+          height={180}
+        />
         <FlexContainer className="flex-col gap-2 absolute bottom-0 bg-modern-tertiary text-white w-full py-2 rounded-b-xl justify-center">
           <span className="text-base font-extrabold">{book.title}</span>
           <span className="text-sm">{book.author}</span>
         </FlexContainer>
-      </div>
+      </BaseCard>
     </FlexContainer>
   );
 };
