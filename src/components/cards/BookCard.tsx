b13f0070f701fe2b847daa9aa910ac2b8621e8ef
Rudolf Cardol <r_cardol@hotmail.com>
2025-12-20 11:33:33 +0000

    add BookCard

---
+++ src/components/cards/BookCard.tsx
@@ -0,0 +1,27 @@
+import { Book } from "@/types/book/book.types";
+import BaseCard from "./BaseCard";
+import NextImage from "../Image";
+import FlexContainer from "../FlexContainer";
+
+interface BookCardProps {
+  book: Book;
+}
+
+const BookCard = ({ book }: BookCardProps) => {
+  return (
+    <BaseCard className="relative lg:w-[287px] lg:h-[393px] overflow-hidden shadow-light-shadow border-none">
+      <NextImage
+        src={book.coverUrl}
+        alt={book.title}
+        width={287}
+        height={393}
+      />
+      <FlexContainer className="flex-col gap-2 absolute bottom-0 bg-modern-tertiary text-white w-full py-2 rounded-b-xl justify-center">
+        <span className="text-base font-extrabold">{book.title}</span>
+        <span className="text-sm">{book.author}</span>
+      </FlexContainer>
+    </BaseCard>
+  );
+};
+
+export default BookCard;
