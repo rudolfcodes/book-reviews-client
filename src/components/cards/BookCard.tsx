fed99c373b27049d6b5441617eab190971bcdfa0
Rudolf Cardol <r_cardol@hotmail.com>
2025-12-20 12:34:23 +0000

    optimize BookCard image

--- src/components/cards/BookCard.tsx
+++ src/components/cards/BookCard.tsx
@@ -11,10 +11,14 @@ const BookCard = ({ book }: BookCardProps) => {
   return (
     <BaseCard className="relative lg:w-[287px] lg:h-[393px] overflow-hidden shadow-light-shadow border-none">
       <NextImage
-        src={book.coverUrl}
+        src={
+          book.coverUrl ||
+          "https://via.placeholder.com/287x393/D3D3D3/000000?text=No+Cover"
+        }
         alt={book.title}
         width={287}
         height={393}
+        loading="lazy"
       />
       <FlexContainer className="flex-col gap-2 absolute bottom-0 bg-modern-tertiary text-white w-full py-2 rounded-b-xl justify-center">
         <span className="text-base font-extrabold">{book.title}</span>
