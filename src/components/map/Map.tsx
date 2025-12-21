bc583be504b3ecb2f45409849819356d3cee3dbd
Rudolf Cardol <r_cardol@hotmail.com>
2025-12-21 17:19:50 +0000

    remove Map container on unmount

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { LatLngExpression, LatLngTuple } from "leaflet";
 import "leaflet/dist/leaflet.css";
@@ -15,27 +15,38 @@ const DEFAULT_ZOOM = 19;
 
 export default function Map({ position, zoom = DEFAULT_ZOOM }: MapProps) {
   const [isClient, setIsClient] = useState(false);
+  const mapIdRef = useRef<string>(`map-${Date.now()}-${Math.random()}`);
 
   useEffect(() => {
     setIsClient(true);
+    // Cleanup function to remove the map container on unmount
+    return () => {
+      const container = document.getElementById(mapIdRef.current);
+      if (container) {
+        container.innerHTML = "";
+      }
+    };
   }, []);
 
   if (!isClient) return null;
 
   return (
-    <MapContainer
-      attributionControl={false}
-      center={position}
-      zoom={zoom}
-      style={{ height: "100%", width: "100%" }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position} draggable={false}>
-        <Popup>Bookclub Location</Popup>
-      </Marker>
-    </MapContainer>
+    <div id={mapIdRef.current} style={{ height: "100%", width: "100%" }}>
+      <MapContainer
+        attributionControl={false}
+        center={position}
+        zoom={zoom}
+        style={{ height: "100%", width: "100%" }}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position} draggable={false}>
+          <Popup>Bookclub Location</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 }
