e90e5c417a8efd5a6eb269fd4cdce4da34797c59
Rudolf Cardol <r_cardol@hotmail.com>
2025-12-21 17:23:34 +0000

    move nav out of carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,8 +1,5 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
-import BaseButton from "./buttons/BaseButton";
-import PrevIcon from "./icons/PrevIcon";
-import NextIcon from "./icons/NextIcon";
 import "swiper/css";
 import "swiper/css/navigation";
 import TitleContainer from "./TitleContainer";
@@ -11,6 +8,7 @@ interface CarouselProps<T> {
   items: T[];
   renderItem: (item: T) => React.ReactNode;
   title?: string;
+  showTitle?: boolean;
   className?: string;
 }
 
@@ -18,42 +16,39 @@ const Carousel = <T,>({
   items,
   renderItem,
   title,
+  showTitle = true,
   className,
 }: CarouselProps<T>) => {
   return (
-    <div className="relative">
-      <div className="flex gap-2 justify-end">
-        {title && (
-          <TitleContainer
-            title={title}
-            className="text-font24 font-bold flex-grow"
-          />
-        )}
+    <div
+      style={{ width: "100%", maxWidth: "100%", minWidth: 0, marginTop: 20 }}
+    >
+      {showTitle && title && (
+        <div className="mb-4">
+          <TitleContainer title={title} className="text-font24 font-bold" />
+        </div>
+      )}
 
-        <BaseButton className="swiper-button-prev-custom" type="button">
-          <PrevIcon />
-        </BaseButton>
-        <BaseButton className="swiper-button-next-custom" type="button">
-          <NextIcon />
-        </BaseButton>
+      <div style={{ width: "100%", overflow: "hidden", minWidth: 0 }}>
+        <Swiper
+          modules={[Navigation]}
+          navigation={{
+            prevEl: ".swiper-button-prev-custom",
+            nextEl: ".swiper-button-next-custom",
+          }}
+          className={className}
+          spaceBetween={16}
+          watchOverflow={false}
+          allowTouchMove={true}
+          slidesPerView="auto"
+        >
+          {items.map((item, index) => (
+            <SwiperSlide key={index} style={{ width: "auto" }}>
+              {renderItem(item)}
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
-
-      <Swiper
-        modules={[Navigation]}
-        navigation={{
-          prevEl: ".swiper-button-prev-custom",
-          nextEl: ".swiper-button-next-custom",
-        }}
-        className={className}
-        spaceBetween={16}
-        slidesPerView="auto"
-      >
-        {items.map((item, index) => (
-          <SwiperSlide key={index} style={{ width: "auto" }}>
-            {renderItem(item)}
-          </SwiperSlide>
-        ))}
-      </Swiper>
     </div>
   );
 };
