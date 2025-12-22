d12c8379aff955c421cd8678f8fd9133423824a0
Rudolf Cardol <r_cardol@hotmail.com>
2025-12-22 13:13:05 +0000

    refactor CurrentlyReading card styles

--- src/components/CurrentlyReading.tsx
+++ src/components/CurrentlyReading.tsx
@@ -11,25 +11,27 @@ interface CurrentlyReadingProps {
 
 const CurrentlyReading = ({ book }: CurrentlyReadingProps) => {
   return (
-    <FlexContainer className="flex-col gap-4 min-w-80">
+    <FlexContainer className="flex-col gap-8 min-w-80 max-h-[480px]">
       <TitleContainer
         title="Currently reading:"
         className="text-font24 font-bold"
       />
 
       <BaseCard className="relative border-10 border-white rounded-custom-lg shadow-input-shadow">
-        <div className="absolute top-right right-0 rounded-tr-custom-lg -mt-[1px] bg-black text-white px-2 py-1 border-t">
-          NOW
+        <div className="absolute top-right right-0 rounded-tr-custom-lg -mt-[1px] bg-black text-white px-2 text-xl py-1 border-t w-16 h-16">
+          <div className="flex h-full justify-center items-center rotate-45 font-bold">
+            NOW
+          </div>
         </div>
         <NextImage
           src={
             book.coverUrl ||
             "https://via.placeholder.com/150x220/D3D3D3/000000?text=No+Cover"
           }
-          className="rounded-custom-lg"
+          className="rounded-custom-lg max-h-[480px]"
           alt={book.title}
-          width={390}
-          height={509}
+          width={350}
+          height={480}
           loading="lazy"
         />
         <div className="gap-1.5 absolute bottom-0 bg-modern-tertiary text-white w-full py-2 px-4 text-center rounded-b-xl justify-center items-center h-20">
