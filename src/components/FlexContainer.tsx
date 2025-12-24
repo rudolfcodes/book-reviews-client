daabda555c7a32f8eb2a3f805f320909b404922b
Rudolf Cardol <r_cardol@hotmail.com>
2025-12-24 19:07:45 +0000

    add inline styles prop to FlexContainer

--- src/components/FlexContainer.tsx
+++ src/components/FlexContainer.tsx
@@ -3,10 +3,15 @@ import React from "react";
 interface FlexContainerProps {
   children: React.ReactNode;
   className?: string;
+  style?: React.CSSProperties;
 }
 
-const FlexContainer = ({ children, className }: FlexContainerProps) => {
-  return <div className={`flex ${className || ""}`}>{children}</div>;
+const FlexContainer = ({ children, className, style }: FlexContainerProps) => {
+  return (
+    <div className={`flex ${className || ""}`} style={style}>
+      {children}
+    </div>
+  );
 };
 
 export default FlexContainer;
