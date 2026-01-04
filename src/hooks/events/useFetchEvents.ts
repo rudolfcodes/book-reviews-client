de41ae5d433971d6dcfccd545b421d8ffd8884d9
Rudolf Cardol <r_cardol@hotmail.com>
2026-01-04 11:29:41 +0100

    refactor to pass correct default value

--- src/hooks/events/useFetchEvents.ts
+++ src/hooks/events/useFetchEvents.ts
@@ -2,10 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchEvents } from "@/services/eventService";
 import { EventApiResponse, EventFilterParams } from "@/types/event/event.types";
 
-const useFetchEvents = (filters: EventFilterParams) => {
+const useFetchEvents = (
+  filters: EventFilterParams = { sortBy: "date", limit: 6 }
+) => {
   const eventsQuery = useQuery<EventApiResponse>({
-    queryKey: ["events", filters || { sortBy: "date", limit: 6 }],
-    queryFn: () => fetchEvents(filters || { sortBy: "date", limit: 6 }),
+    queryKey: ["events", filters],
+    queryFn: () => fetchEvents(filters),
     staleTime: 10 * 60 * 1000,
     refetchOnWindowFocus: false,
   });
