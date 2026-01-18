436aa39bb5aed8b4f41049e30ad7632be284eeb7
Rudolf Cardol <r_cardol@hotmail.com>
2026-01-18 16:17:52 +0100

    added venuetype pills to ClubFilters

--- src/components/clubs/ClubFilters.tsx
+++ src/components/clubs/ClubFilters.tsx
@@ -2,6 +2,8 @@ import { ClubFilterParams } from "@/types";
 import FlexContainer from "../FlexContainer";
 import TitleContainer from "../TitleContainer";
 import CityFilter from "./filters/city/CityFilter";
+import Pill from "./filters/Pill";
+import { useState } from "react";
 
 interface ClubFiltersProps {
   values: ClubFilterParams;
@@ -16,6 +18,15 @@ const ClubFilters = ({
   onReset,
   isLoading,
 }: ClubFiltersProps) => {
+  const venueLabels = {
+    online: "Online",
+    "in-person": "In-Person",
+    hybrid: "Hybrid",
+  } as const;
+  const [selectedVenueType, setSelectedVenueType] = useState<string | null>(
+    null
+  );
+
   if (isLoading) {
     return <div>Loading filters...</div>;
   }
@@ -26,6 +37,26 @@ const ClubFilters = ({
       <TitleContainer title="Filters" className="mb-4" />
       <FlexContainer className="flex-col gap-4">
         <CityFilter filters={values} onChange={onChange} onReset={onReset} />
+        {/* VenueFilter pills */}
+        <FlexContainer className="flex flex-wrap gap-2">
+          {(Object.keys(venueLabels) as Array<keyof typeof venueLabels>).map(
+            (type) => (
+              <Pill
+                key={type}
+                label={venueLabels[type]}
+                isActive={selectedVenueType === type}
+                onClick={() => {
+                  setSelectedVenueType(type);
+                  onChange({
+                    ...values,
+                    venueType: type,
+                  });
+                }}
+              />
+            )
+          )}
+        </FlexContainer>
+        {/* LanguageFilter checkboxes */}
       </FlexContainer>
     </FlexContainer>
   );
